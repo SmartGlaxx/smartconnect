07e1fc308546858147844e99c4d47e7902978f20
SmartGlaxx <Smartegbuchulem@gmail.com>
2022-01-20 23:52:33 +0100

    some ui changes

--- src/Components/Search/search.js
+++ src/Components/Search/search.js
@@ -24,7 +24,7 @@ const Search = ()=>{
                 <div className="search-result"> 
                 <div className="search-item-box">
                     <img src={profilePicture ? profilePicture : ProfileImage} className='search-img'/>
-                    <div>{`${firstname} ${lastname}`}</div>
+                    <div className="search-results">{`${firstname} ${lastname}`}</div>
                 </div>
                 </div>
             </Link>
